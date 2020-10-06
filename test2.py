b46d8aba9299fe2d2b13d4f8399a0d5035a2c9e7
Aniket Mahure <56632536+aniketmahure@users.noreply.github.com>
2020-10-06 12:06:14 +0530

    Update test2.py
    hacktomberfest-accpeted

--- test2.py
+++ test2.py
@@ -4,9 +4,13 @@ import mysql.connector
 mydb3 = mysql.connector.connect(host="localhost", user="root", password="", database="Shop")
 
 root = Tk()
+root.title("Update Items")
+root.resizable(False, False)
+root.overrideredirect(True)
+root.geometry("{0}x{0}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
 
 root.geometry("1920x1080")
-fame = Frame(root, bg="#a64dff").pack()
+fame = Frame(root, bg="red").pack()
 m1cursor = mydb3.cursor()
 sql = "INSERT INTO items (Product_Id,Product_Name, Product_Quantity) VALUES (%s, %s, %s)"
 val = (7, 'A', 20)
