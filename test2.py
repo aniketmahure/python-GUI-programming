94b8fb967258287224e50830a7cd04222066745a
Aniket Mahure <56632536+aniketmahure@users.noreply.github.com>
2020-10-06 12:09:10 +0530

    Update test2.py
    hacktomber-accepted

--- test2.py
+++ test2.py
@@ -4,9 +4,15 @@ import mysql.connector
 mydb3 = mysql.connector.connect(host="localhost", user="root", password="", database="Shop")
 
 root = Tk()
+root.title("Supermarket System : HomePage")
+root.overrideredirect(True)
+root.geometry("{0}x{0}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
+root.resizable(False, False)
+root.configure(background="black")
+
 
 root.geometry("1920x1080")
-fame = Frame(root, bg="#a64dff").pack()
+fame = Frame(root, bg="red").pack()
 m1cursor = mydb3.cursor()
 sql = "INSERT INTO items (Product_Id,Product_Name, Product_Quantity) VALUES (%s, %s, %s)"
 val = (7, 'A', 20)
